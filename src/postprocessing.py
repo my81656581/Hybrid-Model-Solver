2880e7c0daeee0bd08a25346307526f2eb068a81
Shangkun Shen <polossk@126.com>
2019-07-19 20:15:14 +0800

    Update postprocessing.py

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -5,10 +5,14 @@ import sys
 import utils
 
 import reference_basis
+from stiffness import preprocessing_jacobi
+from geometry import xmult
+
+REFERENCE_VERTICES = np.array([[-1, -1], [+1, -1], [-1, +1], [+1, +1]])
 
 
 def get_absolute_displace(displace_field):
-    u_abs = (displace_field[:, 0] ** 2 + displace_field[:, 1] ** 2) ** 0.5
+    u_abs = (displace_field[:, 0]**2 + displace_field[:, 1]**2)**0.5
     print(f"{sys._getframe().f_code.co_name} done.")
     return u_abs
 
@@ -19,36 +23,35 @@ def get_deform_mesh(nodes, displace_field):
     return node_deform
 
 
-def get_strain_field(nodes, elements, basis_config, displace_field):
-    strain_field = np.zeros((len(nodes), 3))
-    frequent = np.zeros((len(nodes), 1))
-    # for i in range(len(elements)):
-    #     xi, yi = [nodes[elements[i, :], _] for _ in range(2)]
-    #     ui, vi = [displace_field[elements[i, :], _] for _ in range(2)]
-    #     for j in range(basis_config.length):
-    #         node_index = elements[i, j]
-    #         epsilon_x, epsilon_y, epsilon_xy = 0, 0, 0
-    #         for k in range(basis_config.length):
-    #             epsilon_x += basis_config.shapes_dx[k](xi[j], yi[j]) * ui[k]
-    #             epsilon_y += basis_config.shapes_dy[k](xi[j], yi[j]) * vi[k]
-    #             epsilon_xy += basis_config.shapes_dx[k](xi[j], yi[j]) * vi[k]
-    #             epsilon_xy += basis_config.shapes_dy[k](xi[j], yi[j]) * ui[k]
-    #         frequent[node_index] += 1
-    #         strain_field[node_index, :] = [epsilon_x, epsilon_y, epsilon_xy]
-    for i in range(len(elements)):
-        xi, yi = [nodes[elements[i, :], _] for _ in range(2)]
-        ui, vi = [displace_field[elements[i, :], _] for _ in range(2)]
+def get_strain_field(nodes, elements, basis, displace_field):
+    # jacobi = [[pxpxg, pxpyg],
+    #           [pypxg, pypyg]]
+    # wanted = [[pxgpx, pygpx],
+    #           [pxgpy, pygpy]]
+    # helper = jacobi^{-1} = [[pxgpx, pxgpy], [pygpx, pygpy]]
+    # pApBg := \frac{\partial A}{\partial B_{gauss}}
+    # pAgpB := \frac{\partial A_{gauss}}{\partial B}
+    n_nodes, n_elements, n_basis = len(nodes), len(elements), basis.length
+    strain_field = np.zeros(shape=(n_nodes, 3))
+    frequent = np.zeros(shape=(n_nodes, 1))
+    xg, yg = REFERENCE_VERTICES.T
+    xg, yg = [np.reshape(_, (-1, 1)) for _ in [xg, yg]]
+    for i in range(n_elements):
+        jacobi = preprocessing_jacobi(xg, yg, nodes[elements[i, :], :], basis)
+        helper = [np.linalg.inv(jacobi[_, :, :]) for _ in range(n_basis)]
+        a, b, c, d = nodes[elements[i, :], :]
         displace_i = displace_field[elements[i, :], :]
-        for j in range(basis_config.length):
-            epsilon_x, epsilon_xy = basis_config.transform(
-                xi[j], yi[j], displace_i, (1, 0))
-            epsilon_y, epsilon_yx = basis_config.transform(
-                xi[j], yi[j], displace_i, (0, 1))
+        aera = np.abs(xmult(b, a, c)) + np.abs(xmult(c, a, d))
+        for j in range(n_basis):
             node_index = elements[i, j]
-            frequent[node_index] += 1
-            strain_field[node_index, :] += [
-                epsilon_x, epsilon_y, epsilon_xy + epsilon_yx
-            ]
+            uxg, vxg = basis.transform(xg[j], yg[j], displace_i, (1, 0))
+            uyg, vyg = basis.transform(xg[j], yg[j], displace_i, (0, 1))
+            ex = helper[j][0, 0] * uxg + helper[j][1, 0] * uyg
+            ey = helper[j][0, 1] * vxg + helper[j][1, 1] * vyg
+            exy = helper[j][0, 0] * vxg + helper[j][1, 0] * vyg
+            eyx = helper[j][0, 1] * uxg + helper[j][1, 1] * uyg
+            frequent[node_index] += aera
+            strain_field[node_index, :] += aera * np.array([ex, ey, exy + eyx])
     strain_field /= frequent
     print(f"{sys._getframe().f_code.co_name} done.")
     return strain_field
@@ -66,13 +69,17 @@ def get_distortion_energy(youngs_modulus, poissons_ratio, stress_field):
     # w= 2 * a^2 + 2 * b^2 - 2 * a * b + 6 * c^2
     a, b, c = [stress_field[:, _] for _ in range(3)]
     coefficient = (1 + poissons_ratio) / 6.0 / youngs_modulus
-    distortion_energy = coefficient * (2 * a * a + 2 * b * b - 2 * a * b + 6 * c * c)
+    distortion_energy = coefficient * (2 * a**2 + 2 * b**2 - 2 * a * b +
+                                       6 * c**2)
     print(f"{sys._getframe().f_code.co_name} done.")
     return distortion_energy
 
 
 def convert_distortion_energy_for_element(distortion_energy, elements):
-    w_element = np.array([np.mean(distortion_energy[elements[_, :]]) for _ in range(len(elements))])
+    w_element = np.array([
+        np.mean(distortion_energy[elements[_, :]])
+        for _ in range(len(elements))
+    ])
     print(f"{sys._getframe().f_code.co_name} done.")
     return w_element
 
@@ -81,7 +88,6 @@ def maximum_distortion_energy_criterion(distortion_energy, w_max):
     w_distortion_index = np.reshape(np.where(distortion_energy > w_max), (-1))
     print(f"{sys._getframe().f_code.co_name} done.")
     return w_distortion_index
-
 
 
 def generate_tecplot_config(n_nodes: int,
@@ -111,7 +117,11 @@ def export_tecplot_data(export: str, config: Dict, nodes, elements,
             f"ZONE N= {config['n_nodes']}, E= {config['n_elements']}, F= FEPOINT, ET= {config['e_type']}",
             file=fout)
         for i in range(config["n_nodes"]):
-            print(f"{nodes[i][0]:6f}", f"{nodes[i][1]:6f}", end='', sep='\t', file=fout)
+            print(f"{nodes[i][0]:6f}",
+                  f"{nodes[i][1]:6f}",
+                  end='',
+                  sep='\t',
+                  file=fout)
             for sol in solutions:
                 for j in range(sol.shape[1]):
                     print('\t', sol[i][j], end='', sep='', file=fout)
