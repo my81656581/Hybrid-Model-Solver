511046b7efd97f8703c1c0bf466bb26beedc9aaf
Shangkun Shen <polossk@126.com>
2019-07-23 22:32:35 +0800

    Update test_PdMaterial2d.py

--- hmsolver/unittest/test_PdMaterial2d.py
+++ hmsolver/unittest/test_PdMaterial2d.py
@@ -22,7 +22,7 @@ class TestPdMaterial2d(unittest.TestCase):
             [a.youngs_modulus, a.poissons_ratio, a.lame_lambda, a.lame_mu])
         answer = np.array([3e11, 1.0 / 3, 2.25e11, 1.125e11])
         ratio = result / answer
-        runs = (1 - 0.005 < ratio).all() or (1 + 0.005 > ratio).all()
+        runs = (1 - 0.005 < ratio).all() and (1 + 0.005 > ratio).all()
         self.assertEqual(runs, True)
 
     def test_material2d_constitutive(self):
@@ -32,15 +32,16 @@ class TestPdMaterial2d(unittest.TestCase):
             np.array([[3.375e+11, 1.125e+11, 0], [1.125e+11, 3.375e+11, 0],
                       [0, 0, 1.125e+11]]), (-1)) + 1e-9
         ratio = result / answer
-        runs = (1 - 0.005 < ratio).all() or (1 + 0.005 > ratio).all()
+        runs = (1 - 0.005 < ratio).all() and (1 + 0.005 > ratio).all()
         self.assertEqual(runs, True)
 
     def test_pd_material2d(self):
-        b = PdMaterial2d(Material2d(3e11, 1.0 / 3))
+        b = PdMaterial2d(3e11, 1.0 / 3)
+        b.setIsotropic(0.06, 0.02, 0.015)
         result = b.coefficients
         answer = np.array([1.24834178e+21, 1.24834178e+21, 9.78995500e+20])
         ratio = result / answer
-        runs = (1 - 0.005 < ratio).all() or (1 + 0.005 > ratio).all()
+        runs = (1 - 0.005 < ratio).all() and (1 + 0.005 > ratio).all()
         self.assertEqual(runs, True)
 
 
