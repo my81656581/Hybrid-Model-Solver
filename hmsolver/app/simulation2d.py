d2cdf2cba7b99b64a76cba14af44eb9f5ec4e039
Shangkun Shen <polossk@126.com>
2019-07-25 21:48:11 +0800

    Update simulation2d.py

--- hmsolver/app/simulation2d.py
+++ hmsolver/app/simulation2d.py
@@ -1,35 +1,39 @@
-import sys
-sys.path.append('./../../')
-
-__all__ = ['PdSimulation2d']
-
 from hmsolver.meshgrid.hybrid_mesh2d import HybridMesh2d
 from hmsolver.material.pd_material2d import PdMaterial2d
-from hmsolver.meshgrid.hybrid_mesh2d import HybridMesh2d
+from hmsolver.app.problem2d import Problem2d, PdProblem2d
+
+__all__ = ['Simulation2d', 'PdSimulation2d']
 
 
-class PdSimulation2d(object):
-    def __init__(self, mesh2d, material2d, bconds):
-        self.__mesh2d_ = mesh2d
-        self.__material2d_ = material2d
-        self.__boundary_conds_ = bconds
-        self.__state_ = [False, False, False]
+class Simulation2d(Problem2d):
+    def startup(self, name):
+        self.__app_name_ = name
+        self._state_ = [False, False, False]
+
+    def _check_mesh(self):
+        self._state_[0] = self.mesh.is_ready()
+        return self._state_[0]
+
+    def _check_material(self):
+        self._state_[1] = self.material.is_ready()
+        return self._state_[1]
+
+    def _check_boundary_conds(self):
+        self._state_[2] = self.boundary_conds.is_ready()
+        return self._state_[2]
 
     def check_engine(self):
         print("*" * 32 + "\nSimulation Manual Checking:")
-        if self.mesh2d != None:
-            # if self.mesh2d.pd_ready():
-            self.__state_[0] = True
+        if self._check_mesh():
+            # if self.mesh2d.is_pdready():
             print("Mesh Ready.")
         else:
             print("Mesh is NOT Ready.")
-        if self.material2d.ready():
-            self.__state_[1] = True
+        if self._check_material():
             print("Material Ready.")
         else:
             print("Material is NOT Ready.")
-        if self.boundary_conds != None:
-            self.__state_[2] = True
+        if self._check_boundary_conds():
             print("Boundary Conds Ready.")
         else:
             print("Boundary Conds is NOT Ready.")
@@ -40,25 +44,18 @@ class PdSimulation2d(object):
         print("*" * 32)
 
     @property
-    def mesh2d(self):
-        return self.__mesh2d_
-
-    @property
-    def material2d(self):
-        return self.__material2d_
-
-    @property
-    def boundary_conds(self):
-        return self.__boundary_conds_
+    def app_name(self):
+        return self.__app_name_
 
     def ready(self) -> bool:
-        return all(self.__state_)
+        return all(self._state_)
 
 
-if __name__ == '__main__':
-    mesh2d = None
-    material2d = PdMaterial2d(3e11, 1.0 / 3)
-    a = PdSimulation2d(mesh2d, material2d, None)
-    a.check_engine()
-    a.material2d.setIsotropic(0.06, 0.02, 0.015)
-    a.check_engine()
+class PdSimulation2d(Simulation2d, PdProblem2d):
+    def _check_mesh(self):
+        self._state_[0] = self.mesh.is_pdready()
+        return self._state_[0]
+
+    def _check_material(self):
+        self._state_[1] = self.material.is_pdready()
+        return self._state_[1]
