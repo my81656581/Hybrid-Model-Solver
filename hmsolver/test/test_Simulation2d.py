942bd07efb206b437572dd514e2884ca566c2325
Shangkun Shen <polossk@126.com>
2019-07-26 18:47:22 +0800

    Update test_Simulation2d.py

--- hmsolver/test/test_Simulation2d.py
+++ hmsolver/test/test_Simulation2d.py
@@ -8,10 +8,12 @@ import hmsolver.geometry as geometry
 from hmsolver.meshgrid.zone2d import Zone2d
 from hmsolver.femcore.preprocessing import read_mesh
 from hmsolver.femcore.treat_boundary import point_criteria, segment_criteria
-from hmsolver.femcore.treat_boundary import boundary_cond2d, Boundary_Conds2d
+from hmsolver.femcore.treat_boundary import boundary_cond2d, BoundaryConds2d
 from hmsolver.app.simulation2d import Simulation2d, PdSimulation2d
 from hmsolver.meshgrid.hybrid_mesh2d import HybridMesh2d
 from hmsolver.material.pd_material2d import PdMaterial2d
+
+from hmsolver.basis.quad4 import Quad4Node
 
 if __name__ == '__main__':
     input_file = 'data_2601_2500.msh'
@@ -43,26 +45,26 @@ if __name__ == '__main__':
     boundary_4 = segment_criteria(zone_xl, zone_yr, zone_xr, zone_yr)
 
     _bc_ = boundary_cond2d  # abbreviate the word for type & read
-    boundarys = Boundary_Conds2d([
+    boundarys = BoundaryConds2d(
         _bc_("point", boundary_0, "fixed", None, None),
         _bc_("segment", boundary_1, "set_uy", "constant", 0),
-        _bc_("segment", boundary_2, "set_ux", "constant", -stretch),
-        _bc_("segment", boundary_3, "set_ux", "constant", +stretch),
-        _bc_("segment", boundary_2, "set_ux",
-             "lambda", lambda x, y: slope * x - 0.5 * stretch),
-        _bc_("segment", boundary_3, "set_ux",
-             "lambda", lambda x, y: slope * x + 1.5 * stretch),
+        # _bc_("segment", boundary_2, "set_ux", "constant", -stretch),
+        # _bc_("segment", boundary_3, "set_ux", "constant", +stretch),
+        # _bc_("segment", boundary_2, "set_ux",
+        #  "lambda", lambda x, y: slope * x - 0.5 * stretch),
+        # _bc_("segment", boundary_3, "set_ux",
+        #  "lambda", lambda x, y: slope * x + 1.5 * stretch),
         _bc_("segment", boundary_4, "set_uy", "constant", tension),
-    ])
+    )
     del _bc_  # delete the abbreviation
 
     a = Simulation2d(mesh2d, material2d, boundarys)
-    a.startup(example_name)
+    a.app_name = example_name
     print(1, end='')
     a.check_engine()
     print("\n\n")
     b = PdSimulation2d(mesh2d, material2d, boundarys)
-    b.startup(example_name)
+    b.app_name = example_name
     print(2, end='')
     b.check_engine()
     b.material.setIsotropic(horizon_radius, grid_size, l_inst)
@@ -76,3 +78,13 @@ if __name__ == '__main__':
     b.boundary_conds.manually_verify()
     print(5, end='')
     b.check_engine()
+
+    c = Simulation2d(None, None, None)
+    d = Simulation2d(None, material2d, boundarys)
+    d = Simulation2d(mesh2d, None, boundarys)
+    d = Simulation2d(mesh2d, material2d, None)
+
+    print('\n' * 3)
+    a.apply_basis(Quad4Node())
+    a.check_engine()
+    u = a.u_solution()
