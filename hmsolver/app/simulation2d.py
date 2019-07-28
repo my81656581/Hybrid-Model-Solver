2c1fd99a19655e5f87fa49a181506fb380a512d2
Shangkun Shen <polossk@126.com>
2019-07-28 19:06:54 +0800

    Update simulation2d.py

--- hmsolver/app/simulation2d.py
+++ hmsolver/app/simulation2d.py
@@ -1,9 +1,11 @@
+import time
+
 from hmsolver.meshgrid.hybrid_mesh2d import HybridMesh2d
 from hmsolver.material.pd_material2d import PdMaterial2d
-from hmsolver.app.problem2d import Problem2d, PdProblem2d
-
 from hmsolver.basis.basisinfo import is_suitable_for_mesh
 from hmsolver.basis.infrastructures import IShapeable
+from hmsolver.app.problem2d import Problem2d, PdProblem2d
+from hmsolver.app.failureprone import ProneRing2d
 
 import hmsolver.femcore.main_procedure as main_procedure
 import hmsolver.femcore.postprocessing as postprocessing
@@ -111,10 +113,15 @@ class Simulation2d(Problem2d):
     def distortion_energy(self):
         return self.w_dis
 
-    @property
-    def u(self):
+    def _selfcheck(self):
         if not self.ready():
             print("Engine is NOT ready. Pls check engine first.")
+            return False
+        return True
+
+    @property
+    def u(self):
+        if not self._selfcheck():
             return None
         if self._u_ is None:
             self._u_ = main_procedure.elasticity(self.mesh, self.material,
@@ -124,8 +131,7 @@ class Simulation2d(Problem2d):
 
     @property
     def u_abs(self):
-        if not self.ready():
-            print("Engine is NOT ready. Pls check engine first.")
+        if not self._selfcheck():
             return None
         if self._u_abs_ is None:
             self._u_abs_ = postprocessing.get_absolute_displace(self.u)
@@ -133,8 +139,7 @@ class Simulation2d(Problem2d):
 
     @property
     def epsilon(self):
-        if not self.ready():
-            print("Engine is NOT ready. Pls check engine first.")
+        if not self._selfcheck():
             return None
         if self._eps_ is None:
             self._eps_ = postprocessing.get_strain_field(
@@ -143,8 +148,7 @@ class Simulation2d(Problem2d):
 
     @property
     def sigma(self):
-        if not self.ready():
-            print("Engine is NOT ready. Pls check engine first.")
+        if not self._selfcheck():
             return None
         if self._sigma_ is None:
             self._sigma_ = postprocessing.get_stress_field(
@@ -153,8 +157,7 @@ class Simulation2d(Problem2d):
 
     @property
     def w_dis(self):
-        if not self.ready():
-            print("Engine is NOT ready. Pls check engine first.")
+        if not self._selfcheck():
             return None
         if self._w_dis_ is None:
             self._w_dis_ = postprocessing.get_distortion_energy(
@@ -194,11 +197,75 @@ class PdSimulation2d(Simulation2d, PdProblem2d):
 
     @property
     def u(self):
-        if not self.ready():
-            print("Engine is NOT ready. Pls check engine first.")
+        if not self._selfcheck():
             return None
         if self._u_ is None:
             self._u_ = main_procedure.peridynamic(self.mesh, self.material,
                                                   self.boundary_conds,
                                                   self.basis)
         return self._u_
+
+
+class CrackSimulation2d(PdSimulation2d):
+    def __init__(self, mesh2d, material2d, bconds):
+        super().__init__(mesh2d, material2d, bconds)
+        self._contains_weight_function_ = []
+        self._n_dgfe_ = 0
+
+    def _selfcheck(self):
+        if not self.ready():
+            print("Engine is NOT ready. Pls check engine first.")
+            return False
+        if not self.material.can_break():
+            print("Crack Simulation SHOULD allow the bond break.")
+            print("Pls set maximun bond stretch first.")
+            print("eg: app.material.stretch_crit = 1.1")
+            return False
+        if not len(self._contains_weight_function_) == self.mesh.n_elements:
+            self._contains_weight_function_ = [
+                False for _ in range(self.mesh.n_elements)
+            ]
+        return True
+
+    @property
+    def u(self):
+        if not self._selfcheck():
+            return None
+        if self._u_ is None:
+            self._u_ = main_procedure.elasticity(self.mesh, self.material,
+                                                 self.boundary_conds,
+                                                 self.basis)
+        return self._u_
+
+    def detect_failure(self, max_distortion_energy):
+        if not self._selfcheck():
+            return None
+        critical_indices = postprocessing.maximum_distortion_energy_criterion(
+            self.w_dis, max_distortion_energy)
+        element_indices = []
+        for c_index in critical_indices:
+            element_indices.extend(self.mesh.adjoint[c_index])
+        for e_index in set(element_indices):
+            self.mesh.manual_set_rule_at_element(e_index)
+            self._contains_weight_function_[e_index] = True
+        return True
+
+    def manual_set_failureprone_zone(self, zone: ProneRing2d):
+        if not self._selfcheck():
+            return None
+        r0, r1 = zone.inner_radius, zone.outer_radius
+        self.mesh.manual_set_rule_at_point(zone.x, zone.y, r0, r1)
+
+    def update_mesh(self):
+        if not self._selfcheck():
+            return None
+        critical = self.mesh.namelist_of_dgfem()
+        self._n_dgfe_ += self.mesh.convert_mesh_into_DGFEM(todolist=critical)
+
+    def run_simulation(self, total_phases=1, max_iter=100):
+        if not self._selfcheck():
+            return None
+        self._u_, self._n_dgfe_ = main_procedure.simulate(
+            self.mesh, self.material, self.boundary_conds, self.basis,
+            (self.app_name, self._n_dgfe_, self._contains_weight_function_),
+            (total_phases, max_iter))
