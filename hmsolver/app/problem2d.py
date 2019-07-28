6349cd89a86e38aa3a9821d6b553e44fd489b22a
Shangkun Shen <polossk@126.com>
2019-07-28 19:06:47 +0800

    Update problem2d.py

--- hmsolver/app/problem2d.py
+++ hmsolver/app/problem2d.py
@@ -9,53 +9,61 @@ __all__ = ['Problem2d', 'PdProblem2d']
 
 class Problem2d(object):
     def __init__(self, mesh2d, material2d, bconds):
-        self.__mesh_ = mesh2d
-        self.__material_ = material2d
-        self.__boundary_conds_ = bconds
-        self.__type_check_result_ = self.type_check()
+        self._mesh_ = mesh2d
+        self._material_ = material2d
+        self._boundary_conds_ = bconds
+        self._msg1_ = ["Mesh", "Material", "Boundary Conds"]
+        self._msg2_ = ["Mesh2d", "Material2d", "BoundaryConds2d"]
+        self._type_check_result_ = self.type_check()
 
     def type_check(self):
-        mesh_ret = isinstance(self.mesh, Mesh2d)
-        mtrl_ret = isinstance(self.material, Material2d)
-        bcnd_ret = isinstance(self.boundary_conds, BoundaryConds2d)
-        msg1 = ["Mesh", "Material", "Boundary Conds"]
-        msg2 = ["Mesh2d", "Material2d", "BoundaryConds2d"]
+        mesh_ret = self.is_mesh_match()
+        mtrl_ret = self.is_material_match()
+        bcnd_ret = self.is_boundary_conds_match()
         flags = [mesh_ret, mtrl_ret, bcnd_ret]
-        for s1, s2, flag in zip(msg1, msg2, flags):
+        for s1, s2, flag in zip(self._msg1_, self._msg2_, flags):
             if not flag:
                 print(f"{s1} instance type ERROR.")
                 print(f"SHOULD BE {s2} object/subclass object.")
-        self.__type_check_result_ = all(flags)
+        self._type_check_result_ = all(flags)
         return all(flags)
 
     @property
     def mesh(self):
-        return self.__mesh_
+        return self._mesh_
 
     @property
     def material(self):
-        return self.__material_
+        return self._material_
 
     @property
     def boundary_conds(self):
-        return self.__boundary_conds_
+        return self._boundary_conds_
 
     def is_defined(self):
-        return self.__type_check_result_
+        return self._type_check_result_
+
+    def is_mesh_match(self):
+        return isinstance(self.mesh, Mesh2d)
+
+    def is_material_match(self):
+        return isinstance(self.material, Material2d)
+
+    def is_boundary_conds_match(self):
+        return isinstance(self.boundary_conds, BoundaryConds2d)
 
 
 class PdProblem2d(Problem2d):
-    def type_check(self):
-        mesh_ret = isinstance(self.mesh, HybridMesh2d)
-        mtrl_ret = isinstance(self.material, PdMaterial2d)
-        bcnd_ret = isinstance(self.boundary_conds, BoundaryConds2d)
-        if not mesh_ret:
-            print("Mesh instance type ERROR.")
-            print("SHOULD BE Mesh2d object/subclass object.")
-        if not mtrl_ret:
-            print("Material instance type ERROR.")
-            print("SHOULD BE Material2d object/subclass object.")
-        if not bcnd_ret:
-            print("Boundary Conds instance type ERROR.")
-            print("SHOULD BE Boundary_Conds2d object/subclass object.")
-        return mesh_ret & mtrl_ret & bcnd_ret
+    def __init__(self, mesh2d, material2d, bconds):
+        self._mesh_ = mesh2d
+        self._material_ = material2d
+        self._boundary_conds_ = bconds
+        self._msg1_ = ["Mesh", "Material", "Boundary Conds"]
+        self._msg2_ = ["HybridMesh2d", "PdMaterial2d", "BoundaryConds2d"]
+        self._type_check_result_ = self.type_check()
+
+    def is_mesh_match(self):
+        return isinstance(self.mesh, HybridMesh2d)
+
+    def is_material_match(self):
+        return isinstance(self.material, PdMaterial2d)
